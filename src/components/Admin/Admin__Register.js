412a3195bebc7e3da67c324f876ba06a00d9e339
SoapyD <tom_bombchild@hotmail.com>
2023-03-09 15:10:12 +0000

    small edit.

--- src/components/Admin/Admin__Register.js
+++ src/components/Admin/Admin__Register.js
@@ -21,7 +21,7 @@ function Admin__Register() {
     
         const genericErrorMessage = "Something went wrong! Please try again later."
 
-        let url = 'eurovision-server-v2.azurewebsites.net:8080'
+        let url = 'https://eurovision-server-v2.azurewebsites.net'
     
         // fetch(process.env.REACT_APP_SERVER_URL + "/register", {
         fetch(url + "/register", {            
